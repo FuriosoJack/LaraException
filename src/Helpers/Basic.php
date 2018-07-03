902db35188a2ecf52ea2e92af7f454e00d6a9a86
FuriosoJack <iam@furiosojack.com>
2018-07-03 14:02:04 -0500

    Se añade helper de laraexception

--- src/Helpers/Basic.php
+++ src/Helpers/Basic.php
@@ -13,3 +13,17 @@ if(!function_exists('path_laraException')){
         return dirname(__DIR__) . ($path ? DIRECTORY_SEPARATOR.$path : $path);
     }
 }
+
+if(!function_exists('lara_exception')){
+    /**
+     * Devuleve la ruta completa de la ubicacion del paquete
+     * @param string $path
+     * @return string
+     */
+    function lara_exception($message)
+    {
+        $lara = new \FuriosoJack\LaraException\ExceptionFather();
+        $lara->message($message);
+        return $lara;
+    }
+}
